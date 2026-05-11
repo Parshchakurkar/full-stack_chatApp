82f2da25e8ad3a24f851f3eb7f329300771868f9
prashant chakurkar <parshchakurkar@gmail.com>
2026-05-11 10:30:53 +0000

    Updated main.tf

--- terraform/modules/acr/main.tf
+++ terraform/modules/acr/main.tf
@@ -7,7 +7,7 @@ resource "azurerm_container_registry" "chat-app-acr" {
   resource_group_name = data.azurerm_resource_group.chat-app.name
   name                = var.acrname
   location            = data.azurerm_resource_group.chat-app.location
-  sku                 = "Basic"
+  sku                 = "Standard"
   #Only Azure RBAC authentication is allowed (recommended)
   admin_enabled                 = false
   public_network_access_enabled = true
